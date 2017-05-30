abc8a6ed072055d95cbd69544766bfc36731026d
Benjamin Daschel <coffeemaxed@gmail.com>
2017-05-29 17:48:31 -0700

    formatting

--- linkedlist.c
+++ linkedlist.c
@@ -8,290 +8,250 @@
 
 #include "linkedlist.h"
 
-void swap(Node * node1, Node * node2);
+void swap(Node *node1, Node *node2);
 
 
-List *  list_create(){
-    
-    List * list  = (List* )calloc(1, sizeof(List));
-    
-    list -> head = (Node * )calloc(1, sizeof(Node));
-    
-    list -> head -> next = list -> head;
-    list -> head -> prev = list -> head;
+List *list_create() {
 
-    list -> size = 0;
-    
+    List *list = (List *) calloc(1, sizeof(List));
+    list->head = (Node *) calloc(1, sizeof(Node));
+
+    list->head->next = list->head;
+    list->head->prev = list->head;
+
+    list->size = 0;
+
     return list;
-
 }
 
-void list_clear(List * list){
+void list_clear(List *list) {
 
-    Node * head = list -> head;
-    
-    Node * cur = head -> next;
-    while(cur != head){
-        
-        if(cur -> data != NULL){
-            free(cur -> data);
+    Node *head = list->head;
+    Node *cur = head->next;
+
+    while (cur != head) {
+
+        if (cur->data != NULL) {
+            free(cur->data);
         }
-        
-        cur = cur -> next;
-        
-        free(cur -> prev);
-        
+        cur = cur->next;
+        free(cur->prev);
     }
+    head->prev = head;
+    head->next = head;
 
-    head -> prev = head;
-    head -> next = head;
-
-    list -> size = 0;
-
+    list->size = 0;
 }
 
-void list_destroy(List * list){
-    
-    list_clear(list);    
+void list_destroy(List *list) {
 
-    free(list -> head);
-    
+    list_clear(list);
+    free(list->head);
     free(list);
-    
 }
 
+void list_add(List *list, void *data) {
 
-void list_add(List * list, void * data){
+    Node *head = list->head;
+    Node *newNode = (Node *) calloc(1, sizeof(Node));
 
-    Node * head = list -> head;
- 
-    Node * newNode = (Node * ) calloc(1, sizeof(Node));
+    newNode->data = data;
+    newNode->next = head;
+    newNode->prev = head->prev;
+    head->prev->next = newNode;
+    head->prev = newNode;
 
-    newNode -> data = data;
-
-    newNode -> next = head;
-    newNode -> prev = head -> prev;
-    head -> prev -> next = newNode;
-    head -> prev = newNode;
-
-    list -> size++;
-
-
+    list->size++;
 }
 
+void list_addFirst(List *list, void *data) {
 
-void list_addFirst(List * list, void * data){
+    Node *head = list->head;
+    Node *newNode = (Node *) calloc(1, sizeof(Node));
 
-    Node * head = list -> head;
- 
-    Node * newNode = (Node * ) calloc(1, sizeof(Node));
+    newNode->data = data;
+    newNode->prev = head;
+    newNode->next = head->next;
+    head->next->prev = newNode;
+    head->next = newNode;
 
-    newNode -> data = data;
-
-    newNode -> prev = head;
-    newNode -> next = head -> next;
-    head -> next -> prev = newNode;
-    head -> next = newNode;
-
-    list -> size++;
-
+    list->size++;
 }
 
-int list_insert(List * list, void * data, const int index){
+int list_insert(List *list, void *data, const int index) {
 
-    if(index >= list_size(list) ){
+    if (index >= list_size(list)) {
         return 1;
     }
 
+    Node *head = list->head;
+    Node *cur = head->next;
+
     int i;
-    Node * head = list->head;
-    Node * cur = head->next;
+    for (i = 0; i < index && cur != head; i++, cur = cur->next);
 
-    for(i = 0; i < index && cur != head; i++, cur = cur->next);
+    Node *newNode = (Node *) calloc(1, sizeof(Node));
 
-    Node * newNode = (Node * ) calloc(1, sizeof(Node));
+    newNode->data = data;
+    newNode->prev = cur->prev;
+    newNode->next = cur;
+    cur->prev->next = newNode;
+    cur->prev = newNode;
 
-    newNode -> data = data;
-
-    newNode -> prev = cur -> prev;
-    newNode -> next = cur;
-    cur -> prev -> next = newNode;
-    cur -> prev = newNode;
-
-    list -> size++;
+    list->size++;
 
     return 0;
-
-
 }
 
-void list_print(List * list, char * (*toString)(void * item) ){
+void list_print(List *list, char *(*toString)(void *item)) {
 
-    Node * head = list -> head;
-    Node * cur;
+    Node *head = list->head;
+    Node *cur;
 
-    for(cur = head -> next; cur != head; cur = cur -> next){
+    for (cur = head->next; cur != head; cur = cur->next) {
 
         printf("%s, ", toString(cur->data));
-
     }
-
 }
 
 
+void *list_get(List *list, int position) {
 
-void * list_get(List * list, int position){
+    assert(position < list_size(list));
 
-    assert( position < list_size(list));
+    Node *head = list->head;
+    Node *cur;
 
-    Node * head = list -> head;
-    Node * cur;
+    for (cur = head->next; position > 0; position--, cur = cur->next);
 
-
-    for(cur = head -> next; position > 0; position--, cur = cur -> next);
-
-    return cur -> data;
-
-
+    return cur->data;
 }
 
+int list_size(List *list) {
 
-int list_size(List * list){
-
-    return list -> size;
-
+    return list->size;
 }
 
-void * list_pop(List * list){
+void *list_pop(List *list) {
 
-    if(list_size(list) < 1){
+    if (list_size(list) < 1) {
         return NULL;
     }
 
-    Node * head = list -> head;
-    Node * cur = head -> prev;
-    void * data = cur -> data;
+    Node *head = list->head;
+    Node *cur = head->prev;
+    void *data = cur->data;
 
-    head -> prev = cur -> prev;
-    cur -> prev -> next = head;
+    head->prev = cur->prev;
+    cur->prev->next = head;
 
     free(cur);
 
-    list -> size--;
+    list->size--;
 
     return data;
 }
 
-
-void list_push(List * list, void * data){
+void list_push(List *list, void *data) {
 
     list_add(list, data);
-
 }
 
+void *list_remove(List *list, int position) {
 
-void * list_remove(List * list, int position){
-
-    if(position < 0 || position > list_size(list)){
+    if (position < 0 || position > list_size(list)) {
         return NULL;
     }
 
-    Node * head = list -> head;
-    Node * cur;
+    Node *head = list->head;
+    Node *cur;
 
-    for(cur = head -> next; position > 0; position--, cur = cur -> next);
+    for (cur = head->next; position > 0; position--, cur = cur->next);
 
-    void * data = cur -> data;
+    void *data = cur->data;
 
-    cur -> prev -> next = cur -> next;
-    cur -> next -> prev = cur -> prev;
+    cur->prev->next = cur->next;
+    cur->next->prev = cur->prev;
 
     free(cur);
 
-    list -> size--;
+    list->size--;
 
     return data;
-
 }
 
-
-void list_enqueue(List * list, void * data){
+void list_enqueue(List *list, void *data) {
 
     list_add(list, data);
-
 }
 
-
-void * list_dequeue(List * list){
+void *list_dequeue(List *list) {
 
     return list_remove(list, 0);
-
 }
 
-int list_indexOf(List * list, void * tofind, int (*cmp)(const void* thing1, const void* thing2) ){
+int list_indexOf(List *list, void *tofind, int (*cmp)(const void *thing1, const void *thing2)) {
 
-    Node * head = list -> head;
+    Node *head = list->head;
 
-    Node * cur;
+    Node *cur;
 
     int index = -1;
     int counter = 0;
 
-    for(cur = head -> next; cur != head; cur = cur -> next){
+    for (cur = head->next; cur != head; cur = cur->next) {
 
-        if(cmp(tofind, cur->data) == 0){
+        if (cmp(tofind, cur->data) == 0) {
             index = counter;
             break;
         }
-
         counter++;
     }
 
     return index;
 }
 
+void swap(Node *node1, Node *node2) {
 
-void swap(Node * node1, Node * node2){
-
-    void * temp = node1->data;
+    void *temp = node1->data;
     node1->data = node2->data;
     node2->data = temp;
 
 }
 
-Iterator * list_iterator(List * list){
+Iterator *list_iterator(List *list) {
 
-    assert( list != NULL);
+    assert(list != NULL);
 
-    Iterator * iter = (Iterator*)calloc(1, sizeof(Iterator));
+    Iterator *iter = (Iterator *) calloc(1, sizeof(Iterator));
 
-    iter -> head = list -> head;
-    iter -> cur = list -> head;
-    iter -> position = -1;
+    iter->head = list->head;
+    iter->cur = list->head;
+    iter->position = -1;
 
     return iter;
 }
 
-void iter_destroy(Iterator * iterator){
+void iter_destroy(Iterator *iterator) {
+
     free(iterator);
 }
 
-void * iter_next(Iterator * iterator){
+void *iter_next(Iterator *iterator) {
 
-    if(iterator -> cur -> next == iterator -> head){
+    if (iterator->cur->next == iterator->head) {
         return NULL;
     }
 
-    iterator -> cur = iterator -> cur -> next;
-    iterator->position ++;
+    iterator->cur = iterator->cur->next;
+    iterator->position++;
 
-    return iterator -> cur -> data;
-
+    return iterator->cur->data;
 }
 
-int iter_hasNext(Iterator * iterator){
+int iter_hasNext(Iterator *iterator) {
 
-    if(iterator -> cur -> next == iterator -> head){
+    if (iterator->cur->next == iterator->head) {
         return 0;
     }
 
--- n2l.c
+++ n2l.c
@@ -2,97 +2,97 @@
 
 #include "n2l.h"
 
-Node * n2l_list_create(){
+Node *n2l_list_create() {
 
-	Node * head = (Node *)calloc(1, sizeof(Node));
+    Node *head = (Node *) calloc(1, sizeof(Node));
 
-	head -> next = head;
-	head -> prev = head;
+    head->next = head;
+    head->prev = head;
 
-	return head;
+    return head;
 }
 
-void n2l_list_add(Node ** head, void * data){
+void n2l_list_add(Node **head, void *data) {
 
-		List * list  = (List * )calloc(1, sizeof(List));
+    List *list = (List *) calloc(1, sizeof(List));
 
-		list -> head = *head;
+    list->head = *head;
 
-		list_add(list, data);
+    list_add(list, data);
 
-		free(list);
-
-}
-
-void n2l_list_clear(Node ** head){
-
-	List * list = (List * )calloc(1, sizeof(list));
-
-	list->head = *head;
-
-	list_clear(list);
-
-	free(list);
+    free(list);
 
 }
 
-void * n2l_list_remove(Node ** head, int index){
+void n2l_list_clear(Node **head) {
 
-	List * list = (List *) calloc(1, sizeof(List));
+    List *list = (List *) calloc(1, sizeof(list));
 
-	list -> head = * head;
+    list->head = *head;
 
-	void * removed = list_remove(list, index);
+    list_clear(list);
 
-	free(list);
-
-	return removed;
-}
-
-int n2l_list_indexOf(Node ** head, void * toFind, int (*cmp)(const void * thing1, const void * thing2)){
-
-	List * list = (List *) calloc(1, sizeof(List));
-
-	list -> head = * head;
-
-	int index = list_indexOf(list, toFind, cmp);
-
-	free(list);
-
-	return index;
+    free(list);
 
 }
 
-void n2l_list_print(Node ** head, char * (*toString)(void * item)){
-	List * list = (List* )calloc(1, sizeof(List));
+void *n2l_list_remove(Node **head, int index) {
 
-	list->head = *head;
+    List *list = (List *) calloc(1, sizeof(List));
 
-	list_print(list, toString);
+    list->head = *head;
 
-	free(list);
+    void *removed = list_remove(list, index);
+
+    free(list);
+
+    return removed;
+}
+
+int n2l_list_indexOf(Node **head, void *toFind, int (*cmp)(const void *thing1, const void *thing2)) {
+
+    List *list = (List *) calloc(1, sizeof(List));
+
+    list->head = *head;
+
+    int index = list_indexOf(list, toFind, cmp);
+
+    free(list);
+
+    return index;
 
 }
 
-void n2l_list_destroy(Node ** head){
+void n2l_list_print(Node **head, char *(*toString)(void *item)) {
+    List *list = (List *) calloc(1, sizeof(List));
 
-	List * list = (List *)calloc(1, sizeof(List));
+    list->head = *head;
 
-	list -> head = * head;
+    list_print(list, toString);
 
-	list_destroy(list);
+    free(list);
 
 }
 
-void n2l_list_sort(Node ** head, int item_size, int(*cmp)( const void * thing1, const void* thing2) ){
+void n2l_list_destroy(Node **head) {
 
-	List * list = (List *)calloc(1, sizeof(List));
+    List *list = (List *) calloc(1, sizeof(List));
 
-	list ->head = *head;
+    list->head = *head;
 
-	list_sort(list, item_size, cmp);
+    list_destroy(list);
 
-	free(list);
+}
+
+void n2l_list_sort(Node **head, int item_size, int(*cmp)(const void *thing1, const void *thing2)) {
+
+    List *list = (List *) calloc(1, sizeof(List));
+
+    list->head = *head;
+
+    list_sort(list, item_size, cmp);
+
+    free(list);
 
 }
 
